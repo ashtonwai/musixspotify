675eef7fae52b15d9053a5c6b38b7d33c94e3226
AsHToN <ashtonwai725@gmail.com>
2014-04-24 10:34:24 -0400

    Initial Commit
    Created project with basic layouts and calls API.

---
+++ scripts/ajax.js
@@ -0,0 +1,47 @@
+"use strict";
+
+// GLOBALS
+var MUSIXMATCH_URL = "http://api.musixmatch.com/ws/1.1/";
+var MUSIXMATCH_API_KEY = "36205906f20a5ef92a7ab93a1ec2f9b5";
+
+// READY FUNCTION
+$(document).ready(function() {
+	$('#searchButton').on('click', function(event) {
+		var search = $('#search').val();
+		getResults(encodeURIComponent(search));
+	}); //  end search button listener
+
+	$('#testLeft').on('click', function(event) {
+		$('#body').toggleClass('leftPanel-open');
+		return false;
+	});
+
+	$('#testRight').on('click', function(event) {
+		$('#body').toggleClass('rightPanel-open');
+		return false;
+	});
+}); // end ready
+
+// FUNCITONS
+function getResults(search) {
+	var url = MUSIXMATCH_URL;
+	url += "track.search?";
+	url += "&q_artist=" + search;
+	url += "&apikey=" + MUSIXMATCH_API_KEY;
+	url += "&format=jsonp";
+	console.log(url);
+
+	$.ajax({
+		url: url,
+		dataType: 'jsonp',
+		success: onJSONLoaded
+	});
+} // end getResults()
+
+function onJSONLoaded(obj) {
+	//console.log(JSON.stringify(obj));
+	var results = obj.message.body.track_list;
+	for (var i=0; i<results.length; i++) {
+
+	}
+}
