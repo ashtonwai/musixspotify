b01e7d41cfc23b17e2f2040826cab8fef8602e00
AsHToN <ashtonwai725@gmail.com>
2014-05-05 10:28:07 -0400

    Edited JavaScript functions
    Displays search results, results layout styled, updates information and
    player.

--- scripts/ajax.js
+++ scripts/ajax.js
@@ -11,14 +11,11 @@ $(document).ready(function() {
 		getResults(encodeURIComponent(search));
 	}); //  end search button listener
 
-	$('#testLeft').on('click', function(event) {
-		$('#body').toggleClass('leftPanel-open');
-		return false;
-	});
-
-	$('#testRight').on('click', function(event) {
-		$('#body').toggleClass('rightPanel-open');
-		return false;
+	$('#search').keypress(function(e) {
+		if (e.keyCode == 13) {
+			var search = $('#search').val();
+			getResults(encodeURIComponent(search));
+		}
 	});
 }); // end ready
 
@@ -29,8 +26,8 @@ function getResults(search) {
 	var searchType = e.options[e.selectedIndex].value;
 	url += "track.search?";
 	url += searchType + search;
-	url += "&apikey=" + MUSIXMATCH_API_KEY;
 	url += "&format=jsonp";
+	url += "&apikey=" + MUSIXMATCH_API_KEY;
 	console.log(url);
 
 	$.ajax({
@@ -42,8 +39,102 @@ function getResults(search) {
 
 function onJSONLoaded(obj) {
 	//console.log(JSON.stringify(obj));
-	var results = obj.message.body.track_list;
-	for (var i=0; i<results.length; i++) {
-		
+	var statusCode = obj.message.header.status_code;
+	switch (statusCode) {
+		case 200:
+			var results = obj.message.body.track_list;
+			var resultsdiv = document.querySelector('#results');
+
+			if (results.length == 0)
+				resultsdiv.innerHTML = "no results found";
+			else {
+				resultsdiv.innerHTML = "";
+				for (var i=0; i<results.length; i++) {
+					var track_id = results[i].track.track_id;
+					var track_name = results[i].track.track_name;
+					var album_name = results[i].track.album_name;
+					var artist_name = results[i].track.artist_name;
+					var coverart = results[i].track.album_coverart_100x100;
+
+					var result = document.createElement('div');
+					var cover = document.createElement('img');
+					var info = document.createElement('div');
+					var title = document.createElement('strong');
+					var album = document.createElement('p');
+					var artist = document.createElement('p');
+					var em = document.createElement('em');
+
+					result.setAttribute('value', track_id);
+					title.innerHTML = track_name;
+					em.innerHTML = album_name;
+					artist.innerHTML = artist_name;
+					cover.setAttribute('src', coverart);
+					
+					resultsdiv.appendChild(result);
+					result.appendChild(cover);
+					result.appendChild(info);
+					info.appendChild(title);
+					album.appendChild(em);
+					info.appendChild(album);
+					info.appendChild(artist);
+
+					result.className = "result";
+					cover.className = "resultAlbum";
+					info.className = "resultInfo";
+
+					result.addEventListener('click', function(e) {
+						var track_id = this.getAttribute('value');
+
+						for (var i=0; i<results.length; i++) {
+							if (results[i].track.track_id == track_id) {
+								var infoTitle = document.querySelector('#infoTitle');
+								var infoAlbum = document.querySelector('#infoAlbum');
+								var infoArtist = document.querySelector('#infoArtist');
+								var coverart = document.querySelector('#albumArt');
+								var player = document.querySelector('#player');
+
+								infoTitle.innerHTML = results[i].track.track_name;
+								infoAlbum.innerHTML = results[i].track.album_name;
+								infoArtist.innerHTML = results[i].track.artist_name;
+								if (results[i].track.album_coverart_350x350 == "") {
+									coverart.setAttribute('src', results[i].track.album_coverart_100x100);
+								} else {
+									coverart.setAttribute('src', results[i].track.album_coverart_350x350);
+								}
+
+								var spotify_id = results[i].track.track_spotify_id;
+								player.setAttribute('src', "https://embed.spotify.com/?uri=spotify:track:" + spotify_id);
+							} // end if
+						} // end for
+					}); // end result listener
+				} // end for
+			} // end else
+			break;
+		case 400:
+			console.log("Error 400 - The request had bad syntax or was inherently impossible to be satisfied.");
+			break;
+		case 401:
+			console.log("Error 401 - Authentication failed, probably because of invalid/missing API key.");
+			break;
+		case 402:
+			console.log("Error 402 - The usage limit has been reached, either you exceeded per day requests limits or your balance is insufficient.");
+			break;
+		case 403:
+			console.log("Error 403 - You are not authorized to perform this operation.");
+			break;
+		case 404:
+			console.log("Error 404 - The requested resource was not found.");
+			break;
+		case 405:
+			console.log("Error 405 - The requested method was not found.");
+			break;
+		case 500:
+			console.log("Error 500 - Ops. Something was wrong.");
+			break;
+		case 503:
+			console.log("Error 503 - Our system is a bit busy at the moment and your request can’t be satisfied.");
+			break;
+		default:
+			console.log("MORE ERRORS!! :(");
 	}
-}
+} // end onJSONLoaded
