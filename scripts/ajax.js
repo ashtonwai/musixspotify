81f68b92787f3d04b96ba9aff3482ca2b10013df
Ashton Wai <ashtonwai725@gmail.com>
2014-05-10 12:28:26 -0400

    Rearranged funtions
    Rearranged functions, breaked onJSONLoaded function into two parts.

--- scripts/ajax.js
+++ scripts/ajax.js
@@ -21,7 +21,7 @@ $(document).ready(function() {
 	});
 }); // end ready
 
-// FUNCITONS
+// AJAX
 function getResults(search) {
 	var url = MUSIXMATCH_URL;
 	var e = document.querySelector('#searchType');
@@ -39,96 +39,41 @@ function getResults(search) {
 	});
 } // end getResults()
 
+function lyricsAJAX(url) {
+	$.ajax({
+		url: url,
+		dataType: 'jsonp',
+		success: function(lyricsObj) {
+			var lyrics = lyricsObj.message.body.lyrics.lyrics_body;
+			var copyright = lyricsObj.message.body.lyrics.lyrics_copyright;
+			var main = document.querySelector('#main');
+			main.innerHTML = lyrics + copyright;
+			//console.log(lyrics);
+		}
+	});
+} // end lyricsAJAX()
+
+function youtubeAJAX(url) {
+	$.ajax({
+		url: url,
+		dataType: 'jsonp',
+		success: function(youtubeObj) {
+			var videoId = youtubeObj.items[0].id.videoId;
+			var video = document.querySelector('#video');
+			video.setAttribute('src', "http://youtube.com/embed/" + videoId);
+		}
+	});
+} // end youtubeAJAX()
+
+
+// FUNCTIONS
 function onJSONLoaded(obj) {
 	//console.log(JSON.stringify(obj));
+
 	var statusCode = obj.message.header.status_code;
 	switch (statusCode) {
 		case 200:
-			var results = obj.message.body.track_list;
-			var resultsdiv = document.querySelector('#results');
-
-			if (results.length == 0)
-				resultsdiv.innerHTML = "no results found";
-			else {
-				resultsdiv.innerHTML = "";
-				for (var i=0; i<results.length; i++) {
-					var track_id = results[i].track.track_id;
-					var track_name = results[i].track.track_name;
-					var album_name = results[i].track.album_name;
-					var artist_name = results[i].track.artist_name;
-					var coverart = results[i].track.album_coverart_100x100;
-
-					var result = document.createElement('div');
-					var cover = document.createElement('img');
-					var info = document.createElement('div');
-					var title = document.createElement('strong');
-					var album = document.createElement('p');
-					var artist = document.createElement('p');
-					var em = document.createElement('em');
-
-					title.innerHTML = track_name;
-					em.innerHTML = album_name;
-					artist.innerHTML = artist_name;
-					cover.setAttribute('src', coverart);
-					result.setAttribute('value', track_id);
-					result.setAttribute('name', artist_name + " " + track_name);
-					
-					resultsdiv.appendChild(result);
-					result.appendChild(cover);
-					result.appendChild(info);
-					info.appendChild(title);
-					album.appendChild(em);
-					info.appendChild(album);
-					info.appendChild(artist);
-
-					result.className = "result";
-					cover.className = "resultAlbum";
-					info.className = "resultInfo";
-
-					result.addEventListener('click', function(e) {
-						var track_id = this.getAttribute('value');
-						var lyricsURL = MUSIXMATCH_URL;
-						lyricsURL += "track.lyrics.get?";
-						lyricsURL += "&track_id=" + track_id;
-						lyricsURL += "&format=jsonp";
-						lyricsURL += "&apikey=" + MUSIXMATCH_API_KEY;
-						//console.log(lyricsURL);
-
-						var youtubeURL = YOUTUBE_URL;
-						youtubeURL += "search?part=snippet";
-						youtubeURL += "&q=" + encodeURIComponent(this.getAttribute('name'));
-						youtubeURL += "&type=video";
-						youtubeURL += "&key=" + YOUTUBE_API_KEY;
-						console.log(youtubeURL);
-
-						for (var i=0; i<results.length; i++) {
-							if (results[i].track.track_id == track_id) {
-								var infoTitle = document.querySelector('#infoTitle');
-								var infoAlbum = document.querySelector('#infoAlbum');
-								var infoArtist = document.querySelector('#infoArtist');
-								var coverart = document.querySelector('#albumArt');
-								var player = document.querySelector('#player');
-
-								var spotify_id = results[i].track.track_spotify_id;
-
-								infoTitle.innerHTML = results[i].track.track_name;
-								infoAlbum.innerHTML = results[i].track.album_name;
-								infoArtist.innerHTML = results[i].track.artist_name;
-								if (results[i].track.album_coverart_350x350 == "") {
-									coverart.setAttribute('src', results[i].track.album_coverart_100x100);
-								} else {
-									coverart.setAttribute('src', results[i].track.album_coverart_350x350);
-								}
-							} // end if
-						} // end for
-
-						player.setAttribute('src', "https://embed.spotify.com/?uri=spotify:track:" + spotify_id);
-						
-						lyricsAJAX(lyricsURL);
-						youtubeAJAX(youtubeURL);
-					}); // end result listener
-				} // end for
-			} // end else
+			returnResults(obj);
 			break;
 		case 400:
 			console.log("Error 400 - The request had bad syntax or was inherently impossible to be satisfied.");
@@ -159,29 +104,88 @@ function onJSONLoaded(obj) {
 	}
 } // end onJSONLoaded
 
-function lyricsAJAX(url) {
-	$.ajax({
-		url: url,
-		dataType: 'jsonp',
-		success: function(lyricsObj) {
-			var lyrics = lyricsObj.message.body.lyrics.lyrics_body;
-			var copyright = lyricsObj.message.body.lyrics.lyrics_copyright;
+function returnResults(obj) {
+	var results = obj.message.body.track_list;
+	var resultsdiv = document.querySelector('#results');
 
-			var main = document.querySelector('#main');
-			main.innerHTML = lyrics + copyright;
-			//console.log(lyrics);
-		}
-	});
-}
+	if (results.length == 0)
+		resultsdiv.innerHTML = "no results found";
+	else {
+		resultsdiv.innerHTML = "";
+		for (var i=0; i<results.length; i++) {
+			var track_id = results[i].track.track_id;
+			var track_name = results[i].track.track_name;
+			var album_name = results[i].track.album_name;
+			var artist_name = results[i].track.artist_name;
+			var coverart = results[i].track.album_coverart_100x100;
 
-function youtubeAJAX(url) {
-	$.ajax({
-		url: url,
-		dataType: 'jsonp',
-		success: function(youtubeObj) {
-			var videoId = youtubeObj.items[0].id.videoId;
-			var video = document.querySelector('#video');
-			video.setAttribute('src', "http://youtube.com/embed/" + videoId);
-		}
-	});
+			var result = document.createElement('div');
+			var cover = document.createElement('img');
+			var info = document.createElement('div');
+			var title = document.createElement('strong');
+			var album = document.createElement('p');
+			var artist = document.createElement('p');
+			var em = document.createElement('em');
+
+			title.innerHTML = track_name;
+			em.innerHTML = album_name;
+			artist.innerHTML = artist_name;
+			cover.setAttribute('src', coverart);
+			result.setAttribute('value', track_id);
+			result.setAttribute('name', artist_name + " " + track_name);
+					
+			resultsdiv.appendChild(result);
+			result.appendChild(cover);
+			result.appendChild(info);
+			info.appendChild(title);
+			album.appendChild(em);
+			info.appendChild(album);
+			info.appendChild(artist);
+
+			result.className = "result";
+			cover.className = "resultAlbum";
+			info.className = "resultInfo";
+
+			result.addEventListener('click', function(e) {
+				var track_id = this.getAttribute('value');
+				var lyricsURL = MUSIXMATCH_URL;
+				lyricsURL += "track.lyrics.get?";
+				lyricsURL += "&track_id=" + track_id;
+				lyricsURL += "&format=jsonp";
+				lyricsURL += "&apikey=" + MUSIXMATCH_API_KEY;
+				//console.log(lyricsURL);
+				lyricsAJAX(lyricsURL);
+
+				var youtubeURL = YOUTUBE_URL;
+				youtubeURL += "search?part=snippet";
+				youtubeURL += "&q=" + encodeURIComponent(this.getAttribute('name'));
+				youtubeURL += "&type=video";
+				youtubeURL += "&key=" + YOUTUBE_API_KEY;
+				//console.log(youtubeURL);
+				youtubeAJAX(youtubeURL);
+
+				for (var i=0; i<results.length; i++) {
+					if (results[i].track.track_id == track_id) {
+						var infoTitle = document.querySelector('#infoTitle');
+						var infoAlbum = document.querySelector('#infoAlbum');
+						var infoArtist = document.querySelector('#infoArtist');
+						var coverart = document.querySelector('#albumArt');
+						var player = document.querySelector('#player');
+
+						var spotify_id = results[i].track.track_spotify_id;
+
+						infoTitle.innerHTML = results[i].track.track_name;
+						infoAlbum.innerHTML = results[i].track.album_name;
+						infoArtist.innerHTML = results[i].track.artist_name;
+						if (results[i].track.album_coverart_350x350 == "") {
+							coverart.setAttribute('src', results[i].track.album_coverart_100x100);
+						} else {
+							coverart.setAttribute('src', results[i].track.album_coverart_350x350);
+						}
+					} // end if
+				} // end for
+				player.setAttribute('src', "https://embed.spotify.com/?uri=spotify:track:" + spotify_id);
+			}); // end result listener
+		} // end for
+	} // end else
 }
